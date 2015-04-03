df50c3ac8b24ecefba840ac02996c026806dc5f4
Chirag Aggarwal <chi6rag@gmail.com>
2015-04-03 16:07:46 +0530

    Add responders gem; add new and create actions, add all_tasks method and before filter

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,26 @@
 class TasksController < ApplicationController
+	before_action :all_tasks, only: [:index, :create]
+	respond_to :html, :js
+
+	def new
+	end
+
+	def create
+		@task = Task.create(params_task)
+	end
+
+	private
+	
+	def all_tasks
+		@tasks = Task.all
+		respond_to do |format|
+			format.html
+			format.js
+		end
+	end
+
+	def params_task
+		params.require(:task).permit(:description, :deadline)
+	end
+
 end
