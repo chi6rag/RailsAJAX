5dfefce2a5ed6a1e23354a4bd63d8237afd348d0
Chirag Aggarwal <chi6rag@gmail.com>
2015-04-03 17:16:39 +0530

    Add create AJAX

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,31 +1,29 @@
 class TasksController < ApplicationController
-	before_action :all_tasks, only: [:index, :create]
-	respond_to :html, :js
+	before_action :all_tasks, only: [:index]
 
 	def new
 		@task = Task.new
-		respond_to do |format|
-			format.html
-			format.js
-		end
 	end
 
 	def create
-		@task = Task.create(params_task)
-	end
-
-	private
-	
-	def all_tasks
 		@tasks = Task.all
+		@task = Task.create(params_task)
 		respond_to do |format|
 			format.html
 			format.js
 		end
 	end
 
-	def params_task
-		params.require(:task).permit(:description, :deadline)
-	end
+	private
+		def all_tasks
+			@tasks = Task.all
+			respond_to do |format|
+				format.html
+				format.json
+			end
+		end
 
+		def params_task
+			params.require(:task).permit(:description, :deadline)
+		end
 end
