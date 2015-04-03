d14aa7b274e2fcebf64478c58fc8227f7e3f37f9
Chirag Aggarwal <chi6rag@gmail.com>
2015-04-03 17:38:52 +0530

    Add edit/update and delete functionality in AJAX

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
 	before_action :all_tasks, only: [:index]
+	before_action :set_task, only: [:edit, :update, :destroy]
 
 	def new
 		@task = Task.new
+	end
+
+	def edit
 	end
 
 	def create
@@ -14,7 +18,29 @@ class TasksController < ApplicationController
 		end
 	end
 
+	def update
+		@tasks = Task.all
+		@task.update_attributes(params_task)
+		respond_to do |format|
+			format.html
+			format.js
+		end
+	end
+
+	def destroy
+		@task.destroy
+		@tasks = Task.all
+		respond_to do |format|
+			format.html
+			format.js
+		end
+	end
+
 	private
+		def set_task
+			@task = Task.find(params[:id])
+		end
+
 		def all_tasks
 			@tasks = Task.all
 			respond_to do |format|
